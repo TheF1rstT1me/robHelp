8c4834500043bd5c5308acb8301ab6fbd18a3e07
Stanislav <ivanivanovix163@gmail.com>
2025-07-03 04:19:59 +0400

    Create MainGui.lua

---
+++ MainGui.lua
@@ -0,0 +1,93 @@
+local RS = game:GetService("ReplicatedStorage")
+local TS = game:GetService("TweenService")
+
+local FolderInstances = RS:FindFirstChild("_CacheEvents")
+
+local GuiObject = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("RobHelper")
+local MainFrame = GuiObject:WaitForChild("MainFrame") :: Frame
+local ShowFrame = GuiObject:WaitForChild("Show") :: Frame
+local HideButton = MainFrame:WaitForChild("Hide") :: GuiButton
+
+local ListContainer = MainFrame:WaitForChild("ListContainer")  :: Frame
+local MainContainer = MainFrame:WaitForChild("MainContainer")  :: Frame 
+local HandlerMainContainer = MainContainer:WaitForChild("HandlerMainContainer") :: Frame
+
+local GUI_OPENED = false
+local currentMenuButton = false
+local TWEENING = false
+local TWEEN_INFO = TweenInfo.new(.5, Enum.EasingStyle.Back, Enum.EasingDirection.InOut)
+
+local function closeGui()
+	if not GUI_OPENED or TWEENING then return end;
+	
+	local closeTween = TS:Create(MainFrame, TWEEN_INFO, {
+		Position = UDim2.fromScale(-1, 0.152)
+	})
+	TWEENING = true
+	closeTween:Play()
+	closeTween.Completed:Wait()
+	MainFrame.Visible = false
+	
+	GUI_OPENED = false
+	ShowFrame.Visible = true
+	TWEENING = false
+end
+
+local function openGui()
+	if GUI_OPENED or TWEENING then return end;
+	
+	MainFrame.Visible = true
+	
+	local openTween = TS:Create(MainFrame, TWEEN_INFO, {
+		Position = UDim2.fromScale(0.08, 0.152)
+	})
+	
+	TWEENING = true
+	openTween:Play()
+	openTween.Completed:Wait()
+	
+	GUI_OPENED = true
+	ShowFrame.Visible = false
+	TWEENING = false
+end
+
+ShowFrame.ShowButton.Activated:Connect(function()
+	openGui()
+end)
+
+HideButton.Activated:Connect(function() 
+	closeGui()
+end)
+
+-- loadstring HANDLERS
+
+for _, Frame_: Frame in pairs(ListContainer:GetChildren()) do
+	if Frame_.Name == "Soon" then continue end;
+	if not Frame_:IsA("Frame") then continue end;
+	
+	local FrameButton = Frame_.Init :: ImageButton
+	local sourceColor = Frame_.BackgroundColor3
+	local selectedColor = Color3.fromRGB(206, 206, 206)
+	
+	FrameButton.Activated:Connect(function() 
+		if currentMenuButton == Frame_ then
+			currentMenuButton = false
+			Frame_.BackgroundColor3 = sourceColor
+			FolderInstances.HandlerMoved:Fire()
+		else
+			if currentMenuButton then
+				currentMenuButton.BackgroundColor3 = sourceColor
+				currentMenuButton = Frame_
+				currentMenuButton.BackgroundColor3 = selectedColor
+				FolderInstances.HandlerMoved:Fire(currentMenuButton.Name)
+			else
+				currentMenuButton = Frame_
+				currentMenuButton.BackgroundColor3 = selectedColor
+				FolderInstances.HandlerMoved:Fire(currentMenuButton.Name)
+			end
+		end
+	end)
+end
+
+ShowFrame.Visible = true
+
