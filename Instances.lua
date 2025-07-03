3d8a33b521aeac5d3fa3f0905fbb0616f5c67bc9
Stanislav <ivanivanovix163@gmail.com>
2025-07-03 04:18:57 +0400

    Create Instances.lua

---
+++ Instances.lua
@@ -0,0 +1,30 @@
+local RS = game:GetService("ReplicatedStorage")
+
+local mainFolder = Instance.new("Folder", RS)
+mainFolder.Name = "_CacheEvents"
+
+local instances = {
+	{
+		Type = "BindableEvent",
+		Name = "HandlerMoved",
+		Parent = game:GetService("ReplicatedStorage"):FindFirstChild("_CacheEvents")
+	},
+	{
+		Type = "BindableEvent",
+		Name = "PosMover",
+		Parent = game:GetService("ReplicatedStorage"):FindFirstChild("_CacheEvents")
+	}
+}
+
+for _, Info in pairs(instances) do
+	task.spawn(function()
+		local obj = Instance.new(Info.Type, Info.Parent)
+		obj.Name = Info.Name
+	end)
+end
+
+while #mainFolder:GetChildren() ~= #instances do
+	task.wait()
+end
+
+-- loadstring
