b3dd4e974d29506f9eb6346a5ef2c6c34f4866e3
Stanislav <ivanivanovix163@gmail.com>
2025-07-03 23:24:20 +0400

    Update AllTogglesLoader.lua

--- Loader/AllTogglesLoader.lua
+++ Loader/AllTogglesLoader.lua
@@ -1,8 +1,8 @@
 local URLS = {
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleMover.lua",
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleNoclip.lua",
---  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleWallhack.lua",
-  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleAntiKnockBack.lua",
+  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleWallhack.lua",
+ -- "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleAntiKnockBack.lua",
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Teleport/Teleport.lua"
 }
 
