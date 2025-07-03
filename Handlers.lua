113093f33401613188517fdaac1b8d19177f9936
Stanislav <ivanivanovix163@gmail.com>
2025-07-03 17:33:37 +0400

    Create Handlers.lua

---
+++ Handlers.lua
@@ -0,0 +1,64 @@
+local RS = game:GetService("ReplicatedStorage")
+local TS = game:GetService("TweenService")
+
+local FolderInstances = RS:WaitForChild("_CacheEvents")
+
+local GuiObject = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("RobHelper")
+local MainFrame = GuiObject:WaitForChild("MainFrame") :: Frame
+local MainContainer = MainFrame:WaitForChild("MainContainer")  :: Frame 
+local HandlerMainContainer = MainContainer:WaitForChild("HandlerMainContainer") :: Frame
+
+local currentHandler = false
+
+local function closeCurrentHandler()
+	if not currentHandler then return end;
+	currentHandler.Visible = false
+	currentHandler = false
+end
+
+local function openNewHandler(name: string)
+	if currentHandler == HandlerMainContainer:FindFirstChild(name) then
+		closeCurrentHandler()
+		return
+	else
+		if currentHandler then
+			closeCurrentHandler()
+			return
+		end
+	end
+	
+	local handler = HandlerMainContainer:FindFirstChild(name)
+	if not handler then return end;
+
+	currentHandler = handler
+	currentHandler.Visible = true
+end
+
+for _, handlerFrame: Frame in pairs(HandlerMainContainer:GetChildren()) do
+	if not handlerFrame:IsA("Frame") then continue end;
+	
+	task.spawn(function()
+		for _, settingFrame: Frame in pairs(handlerFrame:GetChildren()) do
+			if not settingFrame:IsA("Frame") then continue end;
+			
+			task.spawn(function()
+				if settingFrame.Name:match("Toggle") then
+					local button = settingFrame:FindFirstChild("Init", true) :: TextButton
+					button.Activated:Connect(function()
+						FolderInstances:WaitForChild("ToggleChange"):Fire(settingFrame)
+					end)
+				end
+			end)
+			
+		end
+	end)
+end
+
+FolderInstances:WaitForChild("HandlerMoved").Event:Connect(function(handler: string | nil)
+	if not handler then
+		closeCurrentHandler()
+		return
+	end
+	
+	openNewHandler(handler)
+end)
