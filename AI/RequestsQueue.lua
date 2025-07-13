c0fffaf7a5e0277e6c79203cc9635bf1889eae1e
Stanislav <ivanivanovix163@gmail.com>
2025-07-14 02:16:03 +0300

    Update RequestsQueue.lua

--- AI/RequestsQueue.lua
+++ AI/RequestsQueue.lua
@@ -71,7 +71,7 @@ function SendToGemini(tableRequest: {string})
 		local textToChat = responsed.candidates[1].content.parts[1].text
 		print("Ответ:", textToChat)
 		
-		channel:SendAsync(textToChat.." le le le")
+		channel:SendAsync(textToChat.." lelo le le le le")
 	else
 		warn("Ошибка:", response)
 	end
