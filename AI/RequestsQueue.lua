540052ce43a2107a832eec41652ac949862ca7b4
Stanislav <ivanivanovix163@gmail.com>
2025-07-14 00:58:36 +0300

    Create RequestsQueue.lua

---
+++ AI/RequestsQueue.lua
@@ -0,0 +1,97 @@
+local RS = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local FolderInstances = RS:WaitForChild("_CacheEvents")
+local ToggleChanged = FolderInstances:WaitForChild("ToggleChanged") :: BindableEvent
+local AIGemini = FolderInstances:WaitForChild("AIGemini") :: BindableEvent
+local RequestsIsEmpty = FolderInstances:WaitForChild("RequestsIsEmpty") :: BindableFunction
+local SendRequest = FolderInstances:WaitForChild("SendRequest") :: BindableEvent
+local RequestsEmpty = FolderInstances:WaitForChild("RequestsEmpty") :: BindableEvent
+
+local Requested = false
+local Queue = {}
+
+RequestsIsEmpty.OnInvoke = function()
+	return #Queue == 0
+end
+
+function GetQueueIndex(GUID: string)
+	for index, table_ in pairs(Queue) do
+		if table_[4] == GUID then return index end; 
+	end
+	
+	return false
+end
+
+function SendToGemini(tableRequest: {string})
+	if Requested then return end;
+	
+	local model, sysinst, prompt, GUID = table.unpack(tableRequest)
+	local Index = GetQueueIndex(GUID)
+	
+	local data = {
+		contents = {
+			parts = {
+				{
+					role = "user",
+					text = prompt
+				}
+			}
+		},
+		generationConfig = {
+			temperature = 1
+		},
+		safetySettings = {
+			{
+				category = "HATE_SPEECH",
+				threshold = "BLOCK_NONE"
+			}
+		},
+		systemInstruction = {
+			parts = {
+				{
+					text = (sysinst == "" and "Отвечай на русском. Используй слова, какие хочешь, но 100% подходящие в контекст ситуации, желательно: агрессивный стиль." or "Отвечай на русском. Используй слова, какие хочешь, но 100% подходящие в контекст ситуации, желательно: агрессивный стиль. "..sysinst)
+				}
+			}
+		}
+	}
+	
+	local url = `https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent?key=AIzaSyDd6_7zeD3saiD5uu690vXBN-TBrgeqUrE`
+	Requested = true
+	
+	local success, response = pcall(function()
+		return game:HttpPost(
+			url,
+			HttpService:JSONEncode(data),
+			"application/json", -- contentType
+			false
+		)
+	end)
+	
+	if success then
+		print("Ответ:", response)
+	else
+		warn("Ошибка:", response)
+	end
+	
+	Requested = false
+	
+	table.remove(Queue, Index)
+	if #Queue == 0 then
+		RequestsEmpty:Fire()
+	else
+		SendToGemini(Queue[1])
+	end
+end
+
+SendRequest.Event:Connect(function(sysinst, model, prompt) 
+	local GUID = HttpService:GenerateGUID()
+	local newRequest = {model, sysinst, prompt, GUID}
+	table.insert(Queue, newRequest)
+	
+	if #Queue == 1 then
+		SendToGemini(Queue[1])
+	end
+end)
