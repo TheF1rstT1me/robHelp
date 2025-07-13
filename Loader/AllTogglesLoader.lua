909317db507858f576f8d12556996fcdf277698f
Stanislav <ivanivanovix163@gmail.com>
2025-07-14 01:00:37 +0300

    Update AllTogglesLoader.lua

--- Loader/AllTogglesLoader.lua
+++ Loader/AllTogglesLoader.lua
@@ -8,7 +8,9 @@ local URLS = {
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Character/MainCharacter.lua",
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleResetUnlock.lua",
   "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Settings/ToggleBackpackUnlock.lua",
-  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Teleport/Teleport.lua"
+  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AllToggles/Teleport/Teleport.lua",
+  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AI/RequestsQueue.lua",
+  "https://raw.githubusercontent.com/TheF1rstT1me/robHelp/refs/heads/main/AI/AI.lua"
 }
 
 for _, URL: string in pairs(URLS) do
